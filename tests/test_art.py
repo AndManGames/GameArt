f5c7d3b3052fa76f776692631c4dd77c8ef1fe13
AndManGames <schneidera6@googlemail.com>
2023-10-01 20:32:28 +0200

    unittest for art.py added

---
+++ tests/test_art.py
@@ -0,0 +1,28 @@
+from unittest.mock import MagicMock, patch
+
+import matplotlib.pyplot
+import pandas
+
+from gameart.draw import art
+from gameart.utils import utils
+
+
+def test_draw():
+    """
+    Case 1: Mock external dependencies, check if expected values are returned and 
+    verify that matplotlib.pyplot.show() was called to display the plot.
+    """
+    mock_git_root_path = "/mock/git/root/path"
+    mock_csv_file = "/mock/csv/file.csv"
+    mock_data_frame = pandas.DataFrame(
+        {'Key': ['A', 'B', 'C'], 'Duration': [50.0, 75.0, 100.0]})
+
+    with patch('gameart.utils.utils._get_git_root_path', return_value=mock_git_root_path), \
+            patch('gameart.utils.utils._get_latest_csv_file', return_value=mock_csv_file), \
+            patch('pandas.read_csv', return_value=mock_data_frame), \
+            patch('matplotlib.pyplot.show') as mock_show:
+
+        art.draw()
+
+    #
+    assert mock_show.called
