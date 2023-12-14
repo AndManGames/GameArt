7f291045deb7e713a8cc66b9cafdd5703e1da5a9
AndManGames <schneidera6@googlemail.com>
2023-12-14 12:15:52 +0100

    only use list view; have base_folder property, be able to change base folder

--- src/gameart/gui.py
+++ src/gameart/gui.py
@@ -15,7 +15,6 @@ from PyQt5.QtWidgets import (
     QMenu,
     QPushButton,
     QTabWidget,
-    QTreeView,
     QWidget,
 )
 
@@ -31,11 +30,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.file_handler = FileHandlerSingleton()
+        self._base_folder = str(self.file_handler.output_path)
 
         self.setWindowTitle(
             f"GameArt - {importlib.metadata.version('gameart')}"
         )
-        self.resize(750, 500)
+        self.resize(500, 500)
 
         menu_bar = self.menuBar()
         self._create_actions()
@@ -47,70 +47,74 @@ class MainWindow(QMainWindow):
         self._setup_main_tab()
         self.tabWidget.addTab(self.main_tab, "Record and Generate")
 
+    @property
+    def base_folder(self):
+        return self._base_folder
+
+    @base_folder.setter
+    def base_folder(self, value):
+        self._base_folder = value
+
     def _create_actions(self):
-        """Creating action using the first constructor."""
         self.output_folder_action = QAction(self)
         self.output_folder_action.setText("&Select Output Folder")
         self.output_folder_action.triggered.connect(self._select_output_folder)
 
     def _menu_bar(self):
-        """Create the menu bar."""
         file_menu = QMenu("&File", self)
         file_menu.addAction(self.output_folder_action)
 
         return file_menu
 
     def _setup_main_tab(self):
-        """Create the main page."""
         layout = QGridLayout()
 
         # Create buttons
+        btn_select_base_folder = QPushButton("Select Folder")
+        btn_select_base_folder.clicked.connect(self._select_base_folder)
+        layout.addWidget(btn_select_base_folder, 1, 0)
+
         btn_start_record = QPushButton("Start Recording")
         btn_start_record.clicked.connect(self._execute_record)
-        layout.addWidget(btn_start_record, 2, 0, 3, 0)
+        layout.addWidget(btn_start_record, 3, 0)
 
         btn_generate_image = QPushButton("Generate Image")
         btn_generate_image.clicked.connect(self._execute_draw)
-        layout.addWidget(btn_generate_image, 5, 0, 6, 0)
+        layout.addWidget(btn_generate_image, 4, 0)
 
-        # Create file system models (dir and files)
-        self.dir_model = QFileSystemModel()
-        self.dir_model.setRootPath(QDir.rootPath())
-        self.dir_model.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
-
+        # Create file system model
         self.file_model = QFileSystemModel()
         self.file_model.setRootPath(QDir.rootPath())
         self.file_model.setFilter(QDir.NoDotAndDotDot | QDir.Files)
         self.file_model.setNameFilters(["*.csv"])
 
-        # Create tree and list view
-        tree_view = QTreeView()
-        tree_view.setModel(self.dir_model)
-        tree_view.setRootIndex(
-            self.dir_model.index(str(self.file_handler.output_path))
-        )
-        tree_view.clicked.connect(self._on_tree_view_clicked)
-        layout.addWidget(tree_view, 1, 0)
-
+        # Create list view
         self.list_view = QListView()
         self.list_view.setModel(self.file_model)
-        self.list_view.setRootIndex(
-            self.file_model.index(str(self.file_handler.output_path))
-        )
+        self._update_list_view(self.base_folder)
         self.list_view.clicked.connect(self._on_list_view_clicked)
-        layout.addWidget(self.list_view, 1, 1)
+        layout.addWidget(self.list_view, 2, 0)
 
         self.main_tab.setLayout(layout)
 
-    def _on_tree_view_clicked(self, index):
-        folder_path = self.dir_model.fileInfo(index).absoluteFilePath()
-        logging.info("Folder selected %s" % (folder_path))
-        self.list_view.setRootIndex(self.file_model.setRootPath(folder_path))
+    def _update_list_view(self, folder):
+        self.list_view.setRootIndex(self.file_model.setRootPath(folder))
+        logging.info("List view updated.")
 
     def _on_list_view_clicked(self, index):
         file_path = self.file_model.fileInfo(index).absoluteFilePath()
         logging.info("File selected %s" % (file_path))
         self.file_handler.csv_file = file_path
+
+    def _select_base_folder(self):
+        self.base_folder = QFileDialog.getExistingDirectory(
+            self, "Select Folder"
+        )
+        if self.base_folder:
+            self._update_list_view(self.base_folder)
+            logging.info("Folder selected %s" % (self.base_folder))
+        else:
+            logging.warn("No folder selected!")
 
     def _select_output_folder(self):
         output_folder = QFileDialog.getExistingDirectory(
@@ -118,19 +122,22 @@ class MainWindow(QMainWindow):
         )
         if output_folder:
             self.file_handler.output_path = Path(output_folder)
-            logging.info("Folder selected %s" % (output_folder))
+            logging.info("Output folder selected %s" % (output_folder))
         else:
             logging.warn("No output folder selected!")
 
     def _execute_record(self):
         record_mouse()
+        self._update_list_view(self.base_folder)
 
     def _execute_draw(self):
         draw_mouse_tracks()
+        self._update_list_view(self.base_folder)
 
 
 def start_gui():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+
     sys.exit(app.exec_())
