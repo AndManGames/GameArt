981d1da1016cb00de073ef839e7b733da2c6bc05
AndManGames <schneidera6@googlemail.com>
2023-12-15 10:24:19 +0100

    docstrings added

--- src/gameart/gui.py
+++ src/gameart/gui.py
@@ -3,7 +3,7 @@ import logging
 import sys
 from pathlib import Path
 
-from PyQt5.QtCore import QDir
+from PyQt5.QtCore import QDir, QModelIndex
 from PyQt5.QtWidgets import (
     QAction,
     QApplication,
@@ -21,13 +21,20 @@ from PyQt5.QtWidgets import (
 from gameart.api import draw_mouse_tracks, record_mouse
 from gameart.FileHandler import FileHandlerSingleton
 
-logging.basicConfig(level=logging.INFO)
-
 
 class MainWindow(QMainWindow):
-    """Main Window."""
+    """
+    Class of Main Window. Consists of a menu bar and a tab widget.
+    It also uses the singleton file handler class of gameart to get and set the
+    relevant paths. Class has the property base_folder to get and set the base
+    folder which gets displayed in the list view.
 
-    def __init__(self):
+    Args:
+        QMainWindow (object): The QMainWindow class provides a main
+        application window.
+    """
+
+    def __init__(self) -> None:
         super().__init__()
         self.file_handler = FileHandlerSingleton()
         self._base_folder = str(self.file_handler.output_path)
@@ -48,25 +55,40 @@ class MainWindow(QMainWindow):
         self.tabWidget.addTab(self.main_tab, "Record and Generate")
 
     @property
-    def base_folder(self):
+    def base_folder(self) -> str:
         return self._base_folder
 
     @base_folder.setter
     def base_folder(self, value):
         self._base_folder = value
 
-    def _create_actions(self):
+    def _create_actions(self) -> None:
+        """
+        Creates actions for the menu bar buttons. Pressing the button
+        "Select Output Folder" connects to the _select_output_folder method
+        """
         self.output_folder_action = QAction(self)
         self.output_folder_action.setText("&Select Output Folder")
         self.output_folder_action.triggered.connect(self._select_output_folder)
 
-    def _menu_bar(self):
+    def _menu_bar(self) -> QMenu:
+        """
+        Creates the menus for the menu bar and connects the respective actions
+        - File
+
+        Returns:
+            QMenu: Returns the PyQt5 QMenu object
+        """
         file_menu = QMenu("&File", self)
         file_menu.addAction(self.output_folder_action)
 
         return file_menu
 
-    def _setup_main_tab(self):
+    def _setup_main_tab(self) -> None:
+        """
+        Sets up the main tab by creating buttons, file system and list view.
+        It adds all relevant widgets to the layout of the tab.
+        """
         layout = QGridLayout()
 
         # Create buttons
@@ -97,16 +119,43 @@ class MainWindow(QMainWindow):
 
         self.main_tab.setLayout(layout)
 
-    def _update_list_view(self, folder):
+    def _update_list_view(self, folder: str) -> None:
+        """
+        Updates the list view by setting the root index to the given folder
+        path
+
+        Args:
+            folder (str): Path to the folder which shall be visible in list
+            view
+        """
         self.list_view.setRootIndex(self.file_model.setRootPath(folder))
         logging.info("List view updated.")
 
-    def _on_list_view_clicked(self, index):
+    def _on_list_view_clicked(self, index: QModelIndex) -> None:
+        """
+        Gets the file path of the selected file and sets the csv_file property
+        of the file handler class accordingly. If the file is not a .csv the
+        method returns None.
+
+        Args:
+            index (QModelIndex): Index of the file which got selected in list
+            view
+        """
+        file_extension = self.file_model.fileInfo(index).completeSuffix()
+        if file_extension != "csv":
+            logging.warn(
+                "File extension .%s not supported!" % (file_extension)
+            )
+            return
         file_path = self.file_model.fileInfo(index).absoluteFilePath()
         logging.info("File selected %s" % (file_path))
         self.file_handler.csv_file = file_path
 
-    def _select_base_folder(self):
+    def _select_base_folder(self) -> None:
+        """
+        Method to set the base folder of the list view. It updates the list
+        view to display all files in that base folder
+        """
         self.base_folder = QFileDialog.getExistingDirectory(
             self, "Select Folder"
         )
@@ -116,26 +165,39 @@ class MainWindow(QMainWindow):
         else:
             logging.warn("No folder selected!")
 
-    def _select_output_folder(self):
+    def _select_output_folder(self) -> None:
+        """
+        Opens a file dialog to let the user select the output folder, where the
+        gameart output folder will be created after generating the images.
+        """
         output_folder = QFileDialog.getExistingDirectory(
             self, "Select Output Folder"
         )
         if output_folder:
             self.file_handler.output_path = Path(output_folder)
-            logging.info("Output folder selected %s" % (output_folder))
+            print("Output folder selected %s" % (output_folder))
         else:
             logging.warn("No output folder selected!")
 
-    def _execute_record(self):
+    def _execute_record(self) -> None:
+        """
+        Executes the record mouse method and updates the list view afterwards.
+        """
         record_mouse()
         self._update_list_view(self.base_folder)
 
-    def _execute_draw(self):
+    def _execute_draw(self) -> None:
+        """
+        Executes the draw method and updates the list view afterwards.
+        """
         draw_mouse_tracks()
         self._update_list_view(self.base_folder)
 
 
 def _start_gui() -> None:
+    """
+    Starts the GUI.
+    """
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
